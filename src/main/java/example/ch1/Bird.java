c903f68b0480f678a0e8e154d7c8ff7798e1e8f4
Malma <m.almahnna@hotmail.com>
2020-09-14 22:34:49 +0300

    java 11

--- src/main/java/example/ch1/Bird.java
+++ src/main/java/example/ch1/Bird.java
@@ -2,7 +2,15 @@ package example.ch1;
 
 public class Bird {
     public static void main(String[] name) {
-
+    int z=1;
+    String x="c";
+    boolean v=true;
+    double b=1.00;
+    float n=1.00f;
+    long m = 11L;
+    final int tax=2; // %
+        System.out.println("z:"+z + "\n"+"x:"+x +"\n"+"v:"+ v + "\n"+"b:"+b +"\n"+
+                "n:"+n + "\n"+"m:"+m +"\n"+"tax:"+ tax);
        System.out.println("name");
     }
 }
