994c6b3c3d486f671230c49f9fecd2abdad47dcb
Malma <m.almahnna@hotmail.com>
2020-09-16 22:58:45 +0300

    java 15

--- src/main/java/example/ch4/Student.java
+++ src/main/java/example/ch4/Student.java
@@ -4,13 +4,19 @@ public class Student {
     public static void main(String[] args) {
         String name = "M";
         String Email="N";
+        int phoneNo=0555;
+        int extantionNo=3211;
 
         if (name== Email){
 
             System.out.println("error ");
-        }else {
+        }else if (phoneNo==extantionNo){
 
-            System.out.println("OK");
+            System.out.println("phone No can not be the same Ext No");
+        }else {
+            System.out.println("OK, i can see u");
+
+
         }
     }
 }
