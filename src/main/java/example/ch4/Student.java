26cc11ca56c66e4d64e2acc3f13fc0510528442b
Malma <m.almahnna@hotmail.com>
2020-09-16 21:58:47 +0300

    java 11

---
+++ src/main/java/example/ch4/Student.java
@@ -0,0 +1,16 @@
+package example.ch4;
+
+public class Student {
+    public static void main(String[] args) {
+        String name = "M";
+        String Email="N";
+
+        if (name== Email){
+
+            System.out.println("error ");
+        }else {
+
+            System.out.println("OK");
+        }
+    }
+}
