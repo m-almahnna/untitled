7788492214ab7687e3d60ce8118a6a9b540c9f63
Malma <m.almahnna@hotmail.com>
2020-09-19 19:56:15 +0300

    adding SubString

---
+++ src/main/java/example/date/StringSubString.java
@@ -0,0 +1,16 @@
+package example.date;
+
+public class StringSubString {
+    public static void main(String[] args) {
+        String myName = "Mohammed Almahnna";
+
+        String myFirst = myName.substring(0,8);
+
+        System.out.println(myFirst);
+
+        String myLast = myName.substring(9,myName.length());
+
+        System.out.println(myLast);
+
+    }
+}
